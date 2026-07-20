c0c858e650bc45d332936765953b9633e12b4e7c
radeklimek <115090957+radeklimek@users.noreply.github.com>
2026-07-20 13:50:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * wszystkich odwiedzających. Klucz anon jest przeznaczony do użycia w przeglądarce.
  */
 window.LUCKY_WHEAT_SUPABASE = {
-  url: "",
-  anonKey: ""
+  url: "https://acuzzakfancouvrtimzr.supabase.co/rest/v1/",
+  anonKey: "sb_publishable_40Pnzy4PnkJmGtXolx8p1Q_YPCkVh_r"
 };
